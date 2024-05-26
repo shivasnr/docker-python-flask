9170afff5cd349499c2167f6aefd23c594fd6cd7
Shiva Sunar <shivasnr41@gmail.com>
2024-05-26 16:22:03 +0545

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
